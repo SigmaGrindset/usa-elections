f9cd1bc474927e80000ca873c439f577ef366f4d
Antonio Batarilović <antoniobnoni@gmail.com>
2026-05-09 15:24:08 +0200

    c

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-// const prisma = new PrismaClient({ adapter: pool })
+const prisma = new PrismaClient({ adapter: pool })
 
 const app = express()
 
